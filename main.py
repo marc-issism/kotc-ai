a9b6c48141725aa12aeea66314a47aa7ac759c10
Marcus Cua <moojycua@gmail.com>
2024-08-16 20:03:44 -0400

    Set up mistral ai

---
+++ main.py
@@ -0,0 +1,6 @@
+from test import get_response
+# ctrl+shift+p to open up venv
+
+if __name__ == "__main__":
+    print("hello")
+    get_response("Can you write me a song that Lin-Manuel Miranda would write about birds?")
